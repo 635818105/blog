14a73ae0a322616029109aff583c2a85ae727d6e
lml <limenglong@tbkt.cn>
2018-07-13 18:40:14 +0800

    处理归档

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,4 +7,9 @@ from blog.models import Blog
 
 def index(request):
     article_list = Blog.objects.all()
-    return render(request, "blog/index.html", {'articleList': article_list})
+    new_article_list = Blog.objects.order_by("-create_time")[0:3]
+    cursor = connection.cursor()
+    cursor.execute('select date_format(create_time, "%Y年%m月") months from blog_blog group by months;')
+    time_list = cursor.fetchall()
+    # time_list = Blog.objects.raw(r'-- select date_format(create_time, "%Y年%m月") months from blog_blog group by months;')
+    return render(request, "blog/index.html", {'articleList': article_list, "new_article_list": new_article_list, "time_list": time_list})
