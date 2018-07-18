2892f77401e30f51c16b5ba7d3e2beb79b490ee0
lml <limenglong@tbkt.cn>
2018-07-18 19:03:09 +0800

    分类页面处理

--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 
 from blog import views
 
 urlpatterns = [
     url(r'^index/$', views.index),
-    url(r'', views.index),
+    url(r'^$', views.index),
+    url(r'^cate_(\d+)/$', views.cate_handle),
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,17 +17,12 @@ def index(request):
     cursor.execute('select date_format(create_time, "%Y年%m月") months from blog_blog group by months;')
     time_list = cursor.fetchall()
 
-    return render(request, "blog/index.html", {'articleList': article_list, "new_article_list": new_article_list, "time_list": time_list})
-
-
     # 分类
     category_list = Category.objects.all()
     cate_list = list()
     for cate in category_list:
         cate_blog_num = cate.blog_set.all().count()
-        cate = cate.name + "(%d)" % cate_blog_num
-        cate_list.append(cate)
-
+        cate_list.append({"cate": cate, "cate_blog_num": cate_blog_num})
     # 标签
     tag_list = Tag.objects.all()
 
@@ -35,9 +30,32 @@ def index(request):
                "new_article_list": new_article_list,
                "time_list": time_list,
                "cate_list": cate_list,
-               "tag_list": tag_list
+               "tag_list": tag_list,
+               "index": True
                }
-
-
     return render(request, "blog/index.html", content)
 
+
+def cate_handle(request, cate_id):
+    cate_blog_list = Blog.objects.filter(category_id=cate_id).all()
+    cate = Category.objects.get(id=cate_id)
+    cate_blog_num = cate.blog_set.all().count()
+
+    # 时间归档分类
+    cursor = connection.cursor()
+    cursor.execute(
+        'select date_format(create_time, "%Y年%m月") months from blog_blog where category_id = {} group by months;'.format(
+            cate.id))
+    time_list = cursor.fetchall()
+
+    tag_list = list()
+    for blog in cate_blog_list:
+        tag = Tag.objects.filter(blog=blog.id).all()
+        tag_list.extend(tag)
+    content = {"cate_blog_list": cate_blog_list,
+               "cate": cate,
+               "cate_blog_num": cate_blog_num,
+               "tag_list": tag_list,
+               "time_list": time_list
+               }
+    return render(request, "blog/cate_index.html", content)
